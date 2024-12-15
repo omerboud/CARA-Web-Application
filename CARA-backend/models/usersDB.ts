4929485ff159d19487680a9c5f84d4a3d2fdfb34
matanCohenDev <matan209225457@cs.colman.ac.il>
2024-12-15 19:26:34 +0200

    fix user auth

--- CARA-backend/models/usersDB.ts
+++ CARA-backend/models/usersDB.ts
@@ -4,7 +4,14 @@ export interface UserInterface extends mongoose.Document {
     username: string;
     password: string;
     email: string;
+    first_name: string;
+    last_name: string;
+    address: string;
+    phone_number: string;
+    date_of_birth: Date;
+    gender: string;
     productsPosted: Array<string>;
+    productsBought: Array<string>;
     createdAt: Date;
 }
 
@@ -23,7 +30,35 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    first_name:{
+        type: String,
+        required: true
+    },
+    last_name:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    phone_number:{
+        type: String,
+        required: true
+    },
+    date_of_birth:{
+        type: Date,
+        required: true
+    },
+    gender:{
+        type: String,
+        required: true
+    },
     productsPosted:{
+        type: Array,
+        default: []
+    },
+    productsBought:{
         type: Array,
         default: []
     },
